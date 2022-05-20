fe34caf0f4691af91a9254bf98db330a0023d824
llandau <leo_landau@yahoo.com>
2022-05-20 15:56:04 -0700

    working for single letter

--- ll-website/src/MatterComponent.js
+++ ll-website/src/MatterComponent.js
@@ -1,7 +1,20 @@
-import { PureComponent, useEffect, useRef, createRef } from 'react'
-import { Engine, Render, Bodies, World, Runner, Mouse, MouseConstraint, Events } from 'matter-js'
+import { PureComponent, createRef } from 'react';
+import { 
+    Engine, 
+    Render, 
+    Runner,
+    Common,
+    Composite,
+    MouseConstraint,
+    Mouse,
+    Vertices,
+    Bodies, 
+    World,
+    Svg,
+    // Events 
+} from 'matter-js';
 import './MatterComponent.css';
-
+import l_svg from './images/L.svg';
 
 class MatterComponent extends PureComponent {
 
@@ -23,6 +36,16 @@ class MatterComponent extends PureComponent {
     
         console.log("cw: " + cw) // 896
         console.log("ch: " + ch) // 509
+
+
+        //////
+        // required for using Bodies.fromVerticies
+        Common.setDecomp(require('poly-decomp'));
+
+        // required for using Svg.pathToVerticies: https://github.com/progers/pathseg
+        require("pathseg");
+        //////
+
 
         const render = Render.create({
             element: this.scene.current,
@@ -53,16 +76,16 @@ class MatterComponent extends PureComponent {
 
         World.add(engine.world, [
           // walls
-          Bodies.rectangle(cw / 2, -10, cw, 20, {...wallSettings}),
-          Bodies.rectangle(-10, ch / 2, 20, ch, {...wallSettings}),
-          Bodies.rectangle(cw / 2, ch + 10, cw, 20, {...wallSettings}),
-          Bodies.rectangle(cw + 10, ch / 2, 20, ch, {...wallSettings}),
+          Bodies.rectangle(cw / 2, -10, cw, 20, wallSettings),
+          Bodies.rectangle(-10, ch / 2, 20, ch, wallSettings),
+          Bodies.rectangle(cw / 2, ch + 10, cw, 20, wallSettings),
+          Bodies.rectangle(cw + 10, ch / 2, 20, ch, wallSettings),
         ]);
     
         World.add(engine.world, [boxA, boxB, ball]);
     
         // add mouse control
-        var mouse = Mouse.create(render.canvas),
+        const mouse = Mouse.create(render.canvas),
           mouseConstraint = MouseConstraint.create(engine, {
             mouse: mouse,
             constraint: {
@@ -79,15 +102,85 @@ class MatterComponent extends PureComponent {
         //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
         // });
     
-        Engine.run(engine);
-    
+        // run the renderer
         Render.run(render);
+
+        // create runner
+        const runner = Runner.create();
+
+        // run the engine
+        Runner.run(runner, engine);
 
         this.isRunning = true;
 
         this.setState({
             render: render
         })
+        // render.engine.world.bodies
+
+        ///////
+
+        this.addSvgToWorld(engine.world, l_svg);
+
+        ///////
+    }
+
+    toVertices = path => {
+        const pathEl = document.createElementNS(
+          'http://www.w3.org/2000/svg',
+          'path'
+        )
+        pathEl.setAttribute('d', path)
+        return Svg.pathToVertices(pathEl, 2)
+    }
+    
+    toBody = function (letter) {
+        const vertices = this.toVertices(letter)
+      
+        return Bodies.fromVertices(0, 0, Vertices.scale(vertices, 0.8, 0.8), {
+          render: {
+            fillStyle: '#fff',
+            strokeStyle: '#fff',
+            lineWidth: 1,
+          }
+        })
+    }
+    
+    getLetter(letter) {
+        const letters = {
+            "A": 'M 18 114 46 114 70 37 81 74 57 74 51 94 87 94 93 114 121 114 81 7 57 7 z',
+            "U": 'M 16 7 16 82 C 17 125 101 125 99 82 L 99 82 99 7 74 7 74 82 C 73 100 41 99 41 82 L 41 82 41 7 16 7 z',
+            "W": 'M 6 7 29 114 56 114 70 53 84 114 111 114 134 7 108 7 96 74 81 7 59 7 44 74 32 7 6 7 z',
+            "N": 'M 16 114 16 7 42 7 80 74 80 7 106 7 106 114 80 114 42 48 42 114 16 114 z',
+            "P": 'M 20 8 20 114 46 114 46 28 66 28 C 83 28 83 59 66 58 L 66 58 46 58 46 78 67 78 C 116 78 116 7 65 8 L 65 8 z',
+            "D": 'M 19 7 57 7 C 120 13 120 109 57 114 L 57 114 45 114 45 94 56 94 C 85 93 86 30 56 27 L 56 27 45 27 45 114 19 114 19 7 z',
+            "O": 'M 13 59 C 9 -12 109 -12 107 59 L 107 59 80 59 C 84 14 34 14 39 59 L 39 59 C 33 107 86 107 80 59 L 80 59 107 59 C 109 133 9 133 13 59 L 13 59 z',
+            "R": 'M 21 114 21 7 64 7 C 122 8 105 67 85 69 L 85 69 107 113 80 113 61 76 47 76 47 56 65 56 C 84 57 84 26 65 27 L 65 27 47 27 47 114 z',
+            "L": "M 12.354 0 L 12.354 61.23 L 44.629 61.23 L 44.629 71.094 L 0 71.094 L 0 0 L 12.354 0 Z",
+        }
+        return letters[letter.toUpperCase()];
+    }
+
+    addSvgToWorld(world, svg) {
+        const color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+        // const svg_dom = new window.DOMParser().parseFromString(svg, 'image/svg+xml');
+        // const paths = Array.prototype.slice.call(svg_dom.querySelectorAll('path'));
+        // const vertexSets = paths.map((path) => Svg.pathToVertices(path, 30) );
+
+        // const W = 'M 6 7 29 114 56 114 70 53 84 114 111 114 134 7 108 7 96 74 81 7 59 7 44 74 32 7 6 7 z'
+        // const l_set = [l]
+        // const vertexSets = l_set.map((path) => Svg.pathToVertices(path, 30) );
+
+        // const bodies = Bodies.fromVertices(400, 80, vertexSets, {
+        //     render: {
+        //         fillStyle: color,
+        //         strokeStyle: color,
+        //         lineWidth: 1
+        //     }
+        // }, true);
+        // Composite.add(world, bodies);
+
+        Composite.add(world, this.toBody(this.getLetter("O")))
     }
 
     render() {
--- ll-website/src/matterTest.js
+++ ll-website/src/matterTest.js
@@ -1,32 +1,138 @@
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+var Example = Example || {};
 
-// create an engine
-var engine = Engine.create();
+Example.svg = function() {
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Common = Matter.Common,
+        MouseConstraint = Matter.MouseConstraint,
+        Mouse = Matter.Mouse,
+        Composite = Matter.Composite,
+        Vertices = Matter.Vertices,
+        Svg = Matter.Svg,
+        Bodies = Matter.Bodies;
 
-// create a renderer
-var render = Render.create({
-    element: document.body,
-    engine: engine
-});
+    // provide concave decomposition support library
+    Common.setDecomp(require('poly-decomp'));
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    // create engine
+    var engine = Engine.create(),
+        world = engine.world;
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+    // create renderer
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600
+        }
+    });
 
-// run the renderer
-Render.run(render);
+    Render.run(render);
 
-// create runner
-var runner = Runner.create();
+    // create runner
+    var runner = Runner.create();
+    Runner.run(runner, engine);
 
-// run the engine
-Runner.run(runner, engine);
+    // add bodies
+    if (typeof fetch !== 'undefined') {
+        var select = function(root, selector) {
+            return Array.prototype.slice.call(root.querySelectorAll(selector));
+        };
+
+        var loadSvg = function(url) {
+            return fetch(url)
+                .then(function(response) { return response.text(); })
+                .then(function(raw) { return (new window.DOMParser()).parseFromString(raw, 'image/svg+xml'); });
+        };
+
+        ([
+            './svg/iconmonstr-check-mark-8-icon.svg', 
+            './svg/iconmonstr-paperclip-2-icon.svg',
+            './svg/iconmonstr-puzzle-icon.svg',
+            './svg/iconmonstr-user-icon.svg'
+        ]).forEach(function(path, i) { 
+            loadSvg(path).then(function(root) {
+                var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+
+                var vertexSets = select(root, 'path')
+                    .map(function(path) { return Vertices.scale(Svg.pathToVertices(path, 30), 0.4, 0.4); });
+
+                Composite.add(world, Bodies.fromVertices(100 + i * 150, 200 + i * 50, vertexSets, {
+                    render: {
+                        fillStyle: color,
+                        strokeStyle: color,
+                        lineWidth: 1
+                    }
+                }, true));
+            });
+        });
+
+        loadSvg('./svg/svg.svg').then(function(root) {
+            var color = Common.choose(['#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
+            
+            var vertexSets = select(root, 'path')
+                .map(function(path) { return Svg.pathToVertices(path, 30); });
+
+            Composite.add(world, Bodies.fromVertices(400, 80, vertexSets, {
+                render: {
+                    fillStyle: color,
+                    strokeStyle: color,
+                    lineWidth: 1
+                }
+            }, true));
+        });
+    } else {
+        Common.warn('Fetch is not available. Could not load SVG.');
+    }
+
+    Composite.add(world, [
+        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+        Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+    ]);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    Composite.add(world, mouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+        min: { x: 0, y: 0 },
+        max: { x: 800, y: 600 }
+    });
+
+    // context for MatterTools.Demo
+    return {
+        engine: engine,
+        runner: runner,
+        render: render,
+        canvas: render.canvas,
+        stop: function() {
+            Matter.Render.stop(render);
+            Matter.Runner.stop(runner);
+        }
+    };
+};
+
+Example.svg.title = 'Concave SVG Paths';
+Example.svg.for = '>0.16.1';
+
+if (typeof module !== 'undefined') {
+    module.exports = Example.svg;
+}
