1b41b072ca25d07671e52016e20323061a9fd683
llandau <leo_landau@yahoo.com>
2022-05-21 12:16:07 -0700

    cleanup svg stuff

--- ll-website/src/MatterComponent.js
+++ ll-website/src/MatterComponent.js
@@ -2,20 +2,13 @@ import { PureComponent, createRef } from 'react';
 import { 
     Engine, 
     Runner,
-    Common,
-    Composite,
     MouseConstraint,
     Mouse,
-    Vertices,
     Bodies, 
     World,
-    Svg,
-    Constraint
-    // Events 
 } from 'matter-js';
 import { Render } from "./MyRenderer.js";
 import './MatterComponent.css';
-import { letterSvgPaths } from "./constants.js"
 
 class MatterComponent extends PureComponent {
 
@@ -70,7 +63,7 @@ class MatterComponent extends PureComponent {
             friction:0.05,
             density:0.0005,
             render: {
-                fillStyle:"#C44D58",
+                fillStyle: "#C44D58",
                 text:{
                     content: "Test",
                     color: "black",
@@ -123,68 +116,6 @@ class MatterComponent extends PureComponent {
             render: render
         })
         // render.engine.world.bodies
-
-        this.addStrSvgToWorld(engine.world, "abcde");
-    }
-    
-    addStrSvgToWorld(world, str) {
-        // required for using Bodies.fromVerticies
-        Common.setDecomp(require('poly-decomp'));
-
-        // required for using Svg.pathToVerticies: https://github.com/progers/pathseg
-        require("pathseg");
-
-        const color = Common.choose(['#fff', '#f19648', '#f5d259', '#f55a3c', '#063e7b', '#ececd1']);
-
-        // let body = this.charToMatterBody(str, color)
-        // Composite.add(world, body)
-
-        let bodies = []
-        let constraints = []
-        for (let i=0; i<str.length; i++) {
-            let body = this.charToMatterBody(str[i], color)
-            bodies.push(body)
-            // if i==0, add to bodies array, don't add contraint yet
-            if (i === 0) {
-                continue
-            }
-            // add constraints to all prev chars in str
-            for (let j=i-1; j>=0; j--) {
-                let constraint = Constraint.create({
-                    bodyA: bodies[j],
-                    bodyB: body,
-                    length: 42 * (i-j), // multiply by 'distance' from i to j
-                    render: { anchors: false, lineWidth: 0, visible: false },
-                });
-                constraints.push(constraint)
-            }
-        }
-
-        Composite.add(world, [...bodies, ...constraints])
-    }
-
-    charToMatterBody = (str, color) => {
-        const pathEl = document.createElementNS(
-            'http://www.w3.org/2000/svg',
-            'path'
-        )
-        if (letterSvgPaths[str] === undefined) {
-            console.log(str)
-            throw "str not found in letterSvgPaths"
-        }
-
-        const letterPath = letterSvgPaths[str];
-        pathEl.setAttribute('d', letterPath)
-        
-        const verticies = Svg.pathToVertices(pathEl, 2)
-
-        return Bodies.fromVertices(50, 50, Vertices.scale(verticies, 0.8, 0.8), {
-          render: {
-            fillStyle: color,
-            strokeStyle: color,
-            lineWidth: 1,
-          }
-        })
     }
 
     render() {
